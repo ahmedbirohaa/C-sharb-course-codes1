3c9a83e72b608ab411f9b1dad5dbb3b4b42d3468
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-06 04:52:56 +0200

    data type

---
+++ data type/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float flmine = float.MinValue;
+            float flmax = float.MaxValue;
+            Console.WriteLine("Minvalue = {0} Maxvalue = {1}", flmine, flmax);
+            float flVar = 1F / 3;
+            double dlVar = 1D / 3;
+            decimal dcVar = 1M / 3;
+            Console.WriteLine("\nfloat value = {0}\ndouble value = {1}\ndecimal value = {2}", flVar, dlVar, dcVar);
+            Console.ReadKey();
+        }
+    }
+}
