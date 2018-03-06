73a6673705f9ad6a3a9328b0b4fc61d84d6ec2e0
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-06 03:36:13 +0200

    my first variabls

---
+++ my variables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myValue =7;
+
+            Console.WriteLine("{0}", myValue);
+            Console.ReadKey();
+        }
+    }
+}
