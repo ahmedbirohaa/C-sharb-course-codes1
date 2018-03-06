9ec18e0695fa3e8b6c739eec6978345421fe892b
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-06 03:27:15 +0200

    first program
    first  program for c #

---
+++ firstprogram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is my first program
+            Console.WriteLine("Hallo worled");
+            Console.ReadKey();
+
+        }
+    }
+}
