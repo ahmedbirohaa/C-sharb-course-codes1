b9cb0ea99795e2b52ce6c190f014051bd8292c2c
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-07 01:20:32 +0200

    data type char

---
+++ data type char/data type char/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_type_char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int charMain = Char.MinValue;
+            int charMax = char.MaxValue;
+            char Myvar = 'G';
+            int Myvarvalue = 'G';
+            char Myvarvalue2 = Convert.ToChar(75);
+                               
+            Console.WriteLine("charMain = {0} charMax = {1} Mycharacter = {2} Myvarvalue Number = {3} my convert value is = {4} ",
+                 charMain, charMax, Myvar, Myvarvalue, Myvarvalue2);
+
+            string Mystrin = "This is My variable";
+
+            Console.WriteLine("\n My string is {0}", Mystrin);
+
+            bool Mybool = false;
+            Console.WriteLine("\n My bool is {0}", Mybool);
+
+                 Console.ReadKey();
+                
+                                        
+           }
+    }
+}                                    
+                                         
+                                      
+                   
+               
+           
+      
