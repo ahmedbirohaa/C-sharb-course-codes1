ee9f0fbf1a5f1bdb1fd70c86481b049496231939
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-07 12:11:23 +0200

    my string type

---
+++ string type/string type/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  \'  \" \\ \0 \a \b \n \r \t \v \f
+          // @ 
+
+            string Mystring =@"Nazar has said""This is a unique course"", and I think so";
+            string Mystring2 ="\tyas it is";
+            string Mystring3 = Mystring + Mystring2;
+            string Mystring4 = "\n \v \f";
+
+            Console.WriteLine("{0}{1}", Mystring3,Mystring4);
+            Console.ReadKey();
+
+        }
+    }
+}
