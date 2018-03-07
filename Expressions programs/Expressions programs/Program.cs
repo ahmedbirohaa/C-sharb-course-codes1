659c9d33b73349709dd83ce8e8dc4bf0d357ca6b
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-07 12:52:55 +0200

    my expressions

---
+++ Expressions programs/Expressions programs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressions_programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float Myvar = 10.5f;
+            float Myvar2 = 10.7f;
+
+            float temp = Myvar + Myvar2;
+
+            // basic operators  + - * /
+            // bitwise operations 
+            // logical operators boolean true , false
+
+            //1- unary operand
+            //2- binary operands
+            //3- 3 operands
+        }
+    }
+}
