70aed7189393626d1c936fd5e2521535cdaf9708
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-07 11:01:11 +0200

    my literal char

---
+++ Literal char/Literal char/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Literal_char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Myvar = -100;
+            uint Myuvar = 1000u;
+            float Myflvar = 1.5f;
+            double Mydvar = 100.5d;
+            decimal Mydecvar = 100.5m;
+
+            Console.WriteLine("{0} {1}", Myvar, Myuvar);
+            Console.WriteLine($"{Myflvar},{Mydvar},{Mydecvar}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
