3578c6895f93babffaadfd43d88244a0bb1c1b01
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-09 01:33:39 +0200

    التحدي5

---
+++ Increments - Decrements Operations الزياده والنقصان/Increments - Decrements Operations الزياده والنقصان/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Increments___Decrements_Operations_الزياده_والنقصان
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Myvar1 = 5;
+            int Myvar2 = 7;
+            int Myvar3 = 2;
+           int temp1 =  Myvar1 ++ ;
+           // int temp1 = ++Myvar1;
+            int temp2 = Myvar2 --/ ++ Myvar3;
+            Console.WriteLine($"{temp1}\n{temp2}");
+            Console.ReadKey();
+
+        }
+    }
+}
