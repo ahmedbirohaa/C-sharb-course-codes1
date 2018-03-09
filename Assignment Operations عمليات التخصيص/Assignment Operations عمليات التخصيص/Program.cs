1ba0c7ce5b894f7891516828b1658305aa23d31e
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-09 02:19:09 +0200

    التحدي6

---
+++ Assignment Operations عمليات التخصيص/Assignment Operations عمليات التخصيص/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Operations_عمليات_التخصيص
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // =, +=, -=, *=, /=, %=
+
+            int Myvar1 = 7, Myvar2 = 2;
+             Myvar1 /= ++Myvar2;
+          // Myvar1 = Myvar2; // Myvar1 = Myvar2;
+           // Myvar1 += Myvar2; // Myvar1 = Myvar1 + Myvar2;
+           //  Myvar1 -= Myvar2; // Myvar1 = Myvar1 - Myvar2;
+          // Myvar1 *= Myvar2; // Myvar1 = Myvar1 * Myvar2;
+          //  Myvar1 /= Myvar2; // Myvar1 = Myvar1 - Myvar2;
+          //  Myvar1 %= Myvar2; // Myvar1 = Myvar1 % Myvar2;
+
+
+            Console.WriteLine(" {0} \n {1} ", Myvar1, Myvar2);
+            Console.ReadKey();
+               
+        }
+    }
+}
