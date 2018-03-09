aab1acea0acef9530f12f5ff44e0ce55a82c0ea5
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-09 03:30:32 +0200

    التحدي7

---
+++ Boolean Operators المعاملات المنطقية/Boolean Operators المعاملات المنطقية/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Operators_المعاملات_المنطقية
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Boolean comparison ==, !=, <, >, <=, >=
+            // Boolean conditions &&, ||
+            bool Myres;
+            int Myvar1 = 9, Myvar2 = 11;
+           // Myres = Myvar1 != 10;
+            Myres = Myvar1 < 10 || Myvar2 <= 12;
+            Console.WriteLine("{0}", Myres);
+            Console.ReadKey();
+        }
+    }
+}
