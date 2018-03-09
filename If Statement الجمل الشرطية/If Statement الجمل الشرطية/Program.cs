114891d6d2f55a1eded126032e29334a160e7014
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-09 04:39:55 +0200

    التحدي9

---
+++ If Statement الجمل الشرطية/If Statement الجمل الشرطية/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Statement_الجمل_الشرطية
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int Myvar = 11;
+            if (Myvar == 11)
+            {
+                Console.WriteLine("good");
+                Console.WriteLine("{0}", Myvar);
+            }
+
+            else
+                Console.WriteLine("not good");
+
+
+            int Myvar1 = 7, Myvar2 = 9;
+
+            if ((Myvar1 == 8) || (Myvar2 == 9))
+                Console.WriteLine("good");
+            else
+                Console.WriteLine("not good");
+
+            int Myvar = 9;
+            string Myresult;
+            
+                if (Myvar < 10)
+
+              {      Myresult = "less than 10";
+                Console.WriteLine(Myresult);
+            }
+
+
+            if (Myvar > 10)
+
+            {
+                Myresult = "greater than 10";
+                Console.WriteLine(Myresult);
+            }
+
+            if (Myvar == 10)
+
+            {
+                Myresult = "equal 10";
+                Console.WriteLine(Myresult);
+            }
+
+
+
+            int Myvar = 8;
+            if (Myvar > 10)
+                Console.WriteLine("my var is greater than 10");
+            else
+                Console.WriteLine("my var is less than 10");
+
+
+            int Myvar1 = 10, Myvar2 = 5, Myvar3 = 9;
+            if ((Myvar1 <= 11) || (Myvar2 == 5) && (Myvar3 >= 8))
+                Console.WriteLine("true");
+            else
+                Console.WriteLine("false");
+*/
+            int Myvar1 = 10, Myvar2 = 5, Myvar3 = 9;
+            if ((Myvar1 <= 11) || (Myvar2 == 6) && (Myvar3 >= 8))
+                Console.WriteLine("true");
+            else
+                Console.WriteLine("false");
+            Console.ReadKey();
+
+
+
+
+                
+        }
+    }
+}
