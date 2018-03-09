40f4a7401a5e6b533e7d6e80c739072031c9bee2
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-10 01:21:46 +0200

    الشرط الثلاثي

---
+++ Ternary Condition الشرط الثلاثي/Ternary Condition الشرط الثلاثي/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ternary_Condition_الشرط_الثلاثي
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int myvar = 9;
+            // string myresult = (myvar > 10) ? "yes" : "no";
+            // Console.WriteLine("{0}", myresult);
+            // Console.ReadKey();
+
+            string myname = "Ahmed", result;
+            result = (myname.Equals("ahmed")) ? "yes" : "no";
+            Console.WriteLine("{0}", result);
+            Console.ReadKey();
+        }
+    }
+}
