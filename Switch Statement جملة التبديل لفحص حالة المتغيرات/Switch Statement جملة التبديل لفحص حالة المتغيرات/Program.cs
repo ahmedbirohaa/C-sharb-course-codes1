224ec8ae51cac55ebbe2b2b9b09c38b3c007dfc1
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-10 00:48:16 +0200

    التحدي10

---
+++ Switch Statement جملة التبديل لفحص حالة المتغيرات/Switch Statement جملة التبديل لفحص حالة المتغيرات/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Statement_جملة_التبديل_لفحص_حالة_المتغيرات
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*    string myanimal = "cat";
+                switch (myanimal)
+                {
+                    case "cnary":
+                        Console.WriteLine("my animal is cnary");
+                        break;
+                    case "cat":
+                        Console.WriteLine("my animal is cat");
+                        break;
+                    case "camel":
+                        Console.WriteLine("my animal is camel");
+                        break;
+                    default:
+                        Console.WriteLine("no one");
+                        break;
+
+
+                }
+                Console.ReadKey();
+               
+            int myvar = 15;
+            switch (myvar)
+            {
+                case 7:
+                    Console.WriteLine("my var is 7");
+                    break;
+                case 10:
+                    Console.WriteLine("my var is 10");
+                    break;
+                case 20:
+                    Console.WriteLine("my var is 20");
+                    break;
+                default:
+                    Console.WriteLine("no one");
+                    break;
+            }
+            Console.ReadKey();
+*/ 
+               string whathesay = "Hello";
+            switch (whathesay)
+            {
+                case "hello":
+                    Console.WriteLine("hello");
+                    break;
+                case "welcome":
+                    Console.WriteLine("welcome");
+                    break;
+                default:
+                    Console.WriteLine("no one");
+                    break;
+                  
+            }
+                       Console.ReadKey();
+        }
+
+    }
+}
