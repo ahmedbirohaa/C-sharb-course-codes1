55976bf8645b877f72e8fd9369b399d26e7f2a60
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-11 00:06:06 +0200

    التحدي2

---
+++ التحدي رقم2/التحدي رقم2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace التحدي_رقم2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            int myvar1value ='A';
+            int myvar2value = 'a';
+            string mystr1 = "The is my char A value";
+            string mystr2 = "The is my char a value";
+            Console.WriteLine(" {0}  {1}\n {2}  {3}",mystr1, myvar1value,mystr2, myvar2value);
+            Console.ReadKey();
+
+        }
+    }
+}
