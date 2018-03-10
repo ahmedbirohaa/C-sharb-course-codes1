85666fc6ff22ea385f7bca9cffdd7cf9ef79a374
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-11 00:05:00 +0200

    التحدي 13

--- ForLoop Function دالة التكرار الثالثة/ForLoop Function دالة التكرار الثالثة/Program.cs
+++ ForLoop Function دالة التكرار الثالثة/ForLoop Function دالة التكرار الثالثة/Program.cs
@@ -17,14 +17,33 @@ namespace ForLoop_Function_دالة_التكرار_الثالثة
              }
              Console.WriteLine("my salary is {0}", total);
              Console.ReadKey();
-             */
+             
             int mywhith = 120, a = 83, b = 1;
             for (int i = 1; mywhith>a; i++)
             {
                 mywhith -= b;
                 Console.WriteLine("{0}", i);
             }
-           
+                       Console.ReadKey();
+
+
+            int mysalary = 1000, a = 20, b = 2000;
+            for (int i = 0; mysalary<b; i++)
+            {
+                mysalary += a;
+                float years = i / 12;
+                Console.WriteLine("{0}", years);
+            }
+            Console.ReadKey();*/
+
+
+            int a = 120, b = 83, c = 1;
+            for(int i=0; a>b; i--)
+            {
+                a -= c;
+                Console.WriteLine("{0}", i);
+            }
+
             Console.ReadKey();
         }
     }
