5348a6ae796f1d4b61e7e2a8874ec14e6a4ee575
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-10 02:46:35 +0200

    التحدي11

---
+++ Do-While  دالة التكرار الأولى/Do-While  دالة التكرار الأولى/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_While__دالة_التكرار_الأولى
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  double mysalary = 1000.20, total = 0.0;
+              int i = 0;
+              do
+              {
+                  total += mysalary;
+                  i++;
+                  Console.WriteLine("i value eaual {0}", i);
+              }
+              while (i < 10);
+              Console.WriteLine("salary values equal {0}", total);
+              Console.ReadKey();
+              */
+            double a =20, total = 1020.0;
+            int i = 0;
+            do
+            {
+                total += a ;
+                i++;
+                Console.WriteLine("the month of {0}", i);
+            }
+            while (total <= 2000);
+            float years = i / 12;
+            Console.WriteLine("years ={0} ", years);
+            Console.ReadKey();
+        }
+    }
+}
