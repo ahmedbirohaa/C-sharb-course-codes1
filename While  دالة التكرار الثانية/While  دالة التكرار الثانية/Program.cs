6c6eb167553d2ec4aa8b52a4d66f7fbd6d20915d
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-10 12:40:20 +0200

    التحدي12

---
+++ While  دالة التكرار الثانية/While  دالة التكرار الثانية/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While__دالة_التكرار_الثانية
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+{
+            /*  int i = 0;
+              double mysalary = 1020.30, total = 0.0;
+              while (i < 15)
+              {
+                  total += mysalary;
+                  i++;
+
+              }
+              Console.WriteLine("{0}", total);
+              Console.ReadKey();
+              */
+            int i = 0, myweight = 120, a=83;
+            while (myweight>a)
+            {
+                myweight --;
+                i++;
+            }
+            Console.WriteLine("{0}", i);
+            Console.ReadKey();
+
+        }
+    }
+}
