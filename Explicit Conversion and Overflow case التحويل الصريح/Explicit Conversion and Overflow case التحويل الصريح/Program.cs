748a226b1ac47ca8f9a0a7e61ee017f3def22d0d
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-11 04:03:22 +0200

    التحويل الصريح

--- Explicit Conversion and Overflow case التحويل الصريح/Explicit Conversion and Overflow case التحويل الصريح/Program.cs
+++ Explicit Conversion and Overflow case التحويل الصريح/Explicit Conversion and Overflow case التحويل الصريح/Program.cs
@@ -15,14 +15,29 @@ namespace Explicit_Conversion_and_Overflow_case_التحويل_الصريح
               mybyte = (byte)myint;
               Console.WriteLine($"my int = {myint}\n");
               Console.WriteLine($"my byte = {mybyte}");
-              Console.ReadKey(); */
+              Console.ReadKey(); 
 
             byte mybyte;
             int myint = 350;
-            mybyte = (byte)myint;
+            mybyte = unchecked((byte)myint);
             Console.WriteLine($"my int = {myint}\n");
             Console.WriteLine($"my byte = {mybyte}");
             Console.ReadKey();
+
+            decimal mydec;
+            int myint = 350;
+            mydec = myint;
+            Console.WriteLine($"{mydec}");
+            Console.WriteLine($"{myint}");
+            Console.ReadKey();*/
+
+            decimal mydec= 11111111111;
+            int myint ;
+           myint  = unchecked((int) mydec);
+            Console.WriteLine($"{mydec}");
+            Console.WriteLine($"{myint}");
+            Console.ReadKey();
+
         }
     }
 }
