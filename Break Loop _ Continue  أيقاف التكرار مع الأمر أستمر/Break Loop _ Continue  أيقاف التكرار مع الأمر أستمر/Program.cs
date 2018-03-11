16e224a3527b4a7c5219d27d93d105571682fedb
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-11 02:44:53 +0200

    التحدي16

---
+++ Break Loop _ Continue  أيقاف التكرار مع الأمر أستمر/Break Loop _ Continue  أيقاف التكرار مع الأمر أستمر/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break_Loop___Continue__أيقاف_التكرار_مع_الأمر_أستمر
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* for (int i=0;i<=5;i++)
+            {
+                Console.WriteLine("Hello ahmed");
+                if (i > 2)
+                    continue;
+                for(int j=0;j<=4;j++)
+                {
+                    Console.WriteLine("\tHello worled");
+
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();*/
+
+            for(int x=1;x<=10;x++)
+            {
+                Console.WriteLine("The tabel of number {0}", x);
+                if (x > 6)
+                    continue;
+                for(int y=1; y<=10;y++)
+                {
+                    Console.WriteLine("{0} * {1} = {2}", x, y, x * y);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Break Loop _ Continue  أيقاف التكرار مع الأمر أستمر/Break Loop _ Continue  أيقاف التكرار مع الأمر أستمر/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Break Loop _ Continue  أيقاف التكرار مع الأمر أستمر")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Break Loop _ Continue  أيقاف التكرار مع الأمر أستمر")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("66d0404a-cc3d-4fca-b7e9-ebe18567d656")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
