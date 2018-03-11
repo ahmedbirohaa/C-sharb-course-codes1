a5612a0a8b29d6e0dcbee6e94548d7b56b937e74
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-11 03:31:00 +0200

    التحويل الصريح وحالة الفيضان

---
+++ Explicit Conversion and Overflow case التحويل الصريح/Explicit Conversion and Overflow case التحويل الصريح/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explicit_Conversion_and_Overflow_case_التحويل_الصريح
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  byte mybyte;
+              int myint = 7;
+              mybyte = (byte)myint;
+              Console.WriteLine($"my int = {myint}\n");
+              Console.WriteLine($"my byte = {mybyte}");
+              Console.ReadKey(); */
+
+            byte mybyte;
+            int myint = 350;
+            mybyte = (byte)myint;
+            Console.WriteLine($"my int = {myint}\n");
+            Console.WriteLine($"my byte = {mybyte}");
+            Console.ReadKey();
+        }
+    }
+}
