a5b01902e68304b9a3d89e21729c7c5b5b42abe3
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-11 02:22:01 +0200

    التحدي15

---
+++ -Break Loop_Break Command أيقاف التكرار مع الأمر توقف/-Break Loop_Break Command أيقاف التكرار مع الأمر توقف/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Break_Loop_Break_Command_أيقاف_التكرار_مع_الأمر_توقف
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  for(int i = 0; i<=5;i++)
+              {
+                  Console.WriteLine("hello ahmed");
+                  if (i > 2)
+                      break;
+                  for(int j=0; j<=4; j++)
+                  {
+                      Console.WriteLine("\thello worled");
+                  }
+                  Console.WriteLine("\n");
+              }
+              Console.ReadKey(); */
+            for (int x = 1; x <= 10; x++)
+            {
+                if (x > 6)
+                    break;
+            for(int y=1; y<=10; y++)
+                {
+                    Console.WriteLine("{0} * {1} = {2}", x, y, x * y);
+                }
+            }
+            Console.ReadKey();
+                
+
+        }
+    }
+}
