c821d8420b9cd587c8dada47dc4931e11f5bd4e1
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-12 00:25:50 +0200

    التحدي18

---
+++ Convert Command أمر التحويل بين الانواع/Convert Command أمر التحويل بين الانواع/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_Command_أمر_التحويل_بين_الانواع
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  double mydou = 2500.45d;
+              string mystr1, mystr2;
+              //mystr1 = mydou;
+              mystr1 = Convert.ToString(mydou);
+              mystr2 = mystr1.Substring(0, 2);
+              Console.WriteLine($"my doublevar{mydou}");
+              Console.WriteLine("my strinvar{0}", mystr1);
+              Console.WriteLine("my strinvar{0}", mystr2);
+              Console.ReadKey();*/
+            float myfloat = 45.56f;
+            string mystr1, mystr2;
+            mystr1 = Convert.ToString(myfloat);
+            mystr2 = mystr1.Substring(2);
+            Console.WriteLine($"{myfloat}\n{mystr1}\n{mystr2}");
+            Console.ReadKey();
+
+        }
+    }
+}
