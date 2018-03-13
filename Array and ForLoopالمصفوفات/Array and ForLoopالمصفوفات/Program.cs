a95177e2b736512ba4070b8610fdc2cd412d37d1
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-13 11:42:13 +0200

    التحدي22

---
+++ Array and ForLoopالمصفوفات/Array and ForLoopالمصفوفات/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_and_ForLoopالمصفوفات
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* const int myarraysiz = 5;
+
+             int[] mynumber = { 1, 5, 8, 9, 7 };
+             int[] mynumber2 = new int [myarraysiz];
+             int[] mynumber3 = new int[myarraysiz] { 1, 5, 8, 9, 7 };
+
+             Console.WriteLine($"{mynumber[4]}");
+             for (int i = 0; i< 5; i++) 
+             Console.WriteLine($" my number {(i + 1)} is {mynumber3[i]}");
+             Console.ReadKey();*/
+
+            int[] mynumber = { 1, 3, 5, 7, 9,11,13,15 };
+            Console.WriteLine($"{mynumber.Max()}");
+            Console.ReadKey();
+        }
+    }
+}
