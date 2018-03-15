85bcefc3ad7d839f2140151e252a2bc88cb36312
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-16 01:14:22 +0200

    المصفوفات النصية

---
+++ Array Of String and Array Length المصفوفات النصية/Array Of String and Array Length المصفوفات النصية/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Of_String_and_Array_Length_المصفوفات_النصية
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] myfullname = { "ahmed", "mohamed","nabil", "shmah"};
+            int myarraylength = myfullname.Length;
+
+            for (int i = 0; i<myarraylength; i++)
+            {
+                Console.Write($"{myfullname[i]}" + " ");
+            }
+            Console.WriteLine($"\nmy array lenght is {myarraylength}");
+            Console.ReadKey();
+
+        }
+    }
+}
