c4a4753ed7d2d2b285d5e56c6b3ca4e8ee1b1acd
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-18 02:32:51 +0200

    التحدي25

---
+++ String Manipulation Operationsالمصفوفات الحرفية/String Manipulation Operationsالمصفوفات الحرفية/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Manipulation_Operationsالمصفوفات_الحرفية
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string mystr = "ahmed shmah";
+            //char[] mychararry = mystr.ToCharArray();
+
+            //Console.WriteLine($"my array lgh is {mychararry.Length}");
+            
+            //for (int i = 0;  i < mychararry.Length; i++)
+            //{
+            //    Console.Write($"{mychararry[i]}");
+            //}
+
+            //foreach ( char myname in mychararry)
+            //{
+            //    Console.WriteLine($"{myname}");
+            //}
+
+            string mystr1 = "i am smart";
+            
+
+            char[] mychararry = mystr1.ToCharArray();
+
+            Console.Write("he is ");   
+            for(int i=5; i<mychararry.Length; i++)
+            {
+
+                Console.Write($"{mychararry[i]}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
