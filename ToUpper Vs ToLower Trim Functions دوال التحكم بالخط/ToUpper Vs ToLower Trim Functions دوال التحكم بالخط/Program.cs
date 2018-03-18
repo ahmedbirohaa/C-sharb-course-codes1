7e2fa1790e35042cf0c2d068410a153d3515d840
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-18 03:10:59 +0200

    دوال التحكم بالخط مع النصوص وحذف الحروف الغير مرغوبة

---
+++ ToUpper Vs ToLower Trim Functions دوال التحكم بالخط/ToUpper Vs ToLower Trim Functions دوال التحكم بالخط/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToUpper_Vs_ToLower_Trim_Functions_دوال_التحكم_بالخط
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystr1, mystr2, mystr3,mystr4;
+            char[] mytrimchar = { ' ', 'a', 's' }; 
+            Console.WriteLine("ples inter yore string");
+            mystr1 = Console.ReadLine();
+            mystr2 = mystr1.ToUpper();
+            mystr3 = mystr1.ToLower();
+            mystr4 = mystr3.Trim(mytrimchar);
+            Console.WriteLine($"{mystr2}\n{mystr3}\n{mystr4}");
+            Console.ReadKey();
+        }
+    }
+}
