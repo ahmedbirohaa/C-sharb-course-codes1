c373e9ea098c3253b61c6b070ebacacfd198687a
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-18 11:55:48 +0200

    الداله الاولي

---
+++ First Function كتابة الدالة الأولى بلغة السي شارب/First Function كتابة الدالة الأولى بلغة السي شارب/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Function_كتابة_الدالة_الأولى_بلغة_السي_شارب
+{
+    class Program
+    {
+        static void print_name()
+        {
+            Console.WriteLine("ahmed");
+        }
+        static void Main(string[] args)
+        {
+            print_name();
+            Console.ReadKey();
+        }
+    }
+}
