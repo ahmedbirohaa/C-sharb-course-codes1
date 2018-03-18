96fdc07bbd07c5f747c39908ab4411787b28c91a
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-18 11:28:26 +0200

    تحويل النصوص الي كلمات

---
+++ Split Function with String Type تحويل النصوص الى كلمات/Split Function with String Type تحويل النصوص الى كلمات/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split_Function_with_String_Type_تحويل_النصوص_الى_كلمات
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystr;
+            char[] spretor = { ' ','u' };
+            Console.WriteLine("enter your string");
+            mystr = Console.ReadLine();
+            string[] newword;
+            newword = mystr.Split(spretor);
+            foreach (string word in newword)
+            {
+              Console.WriteLine($"{word}");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
