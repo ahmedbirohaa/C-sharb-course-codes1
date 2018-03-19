550c0db9ffc8d671db0cbe0498a109497c51667f
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-19 04:27:50 +0200

    مدي المتغيرات

---
+++ Variables Scope مدى المتغيرات وامكانية الوصول/Variables Scope مدى المتغيرات وامكانية الوصول/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables_Scope_مدى_المتغيرات_وامكانية_الوصول
+{
+    class Program
+    {
+        static string mystring;
+        static void print()
+        {
+            string mystring = " it is my string print";
+            Console.WriteLine($"{mystring}");
+            Console.WriteLine($"{Program.mystring}");
+        }
+        static void Main(string[] args)
+        {
+            string mystring = " it is my string";
+            Console.WriteLine($"{mystring}");
+             Program.mystring = "it is my string programs";
+            print();
+            Console.WriteLine($"{Program.mystring}");
+
+            Console.ReadKey();
+        }
+    }
+}
