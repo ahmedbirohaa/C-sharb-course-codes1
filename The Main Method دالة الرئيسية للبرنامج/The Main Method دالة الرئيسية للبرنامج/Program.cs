fe0f54165d37940b7bc53aaa17d62c019c7d86de
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-19 04:58:08 +0200

    الداله الرئيسيه

---
+++ The Main Method دالة الرئيسية للبرنامج/The Main Method دالة الرئيسية للبرنامج/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Main_Method_دالة_الرئيسية_للبرنامج
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine($"{args.Length}");
+            foreach( string a in args)
+            {
+                Console.WriteLine($"{a}");
+            }
+            Console.ReadKey();
+            return 0;
+        }
+        
+    }
+}
