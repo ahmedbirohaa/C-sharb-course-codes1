0fa1dd331a0ecfc386b1a2165ac33f558d59ed5b
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-19 02:25:53 +0200

    التحدي27

---
+++ Reference values  القيم المرجعية/Reference values  القيم المرجعية/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reference_values__القيم_المرجعية
+{
+    class Program
+    {
+        static void multeb_number (ref int mynumber1,ref int mynmber2)
+        {
+            mynumber1 *= 4;
+            mynmber2 *= 4;
+            Console.WriteLine($"{mynumber1} {mynmber2}");
+        }
+        static void Main(string[] args)
+        {
+            int mynumber1 = 5;
+            int mynumber2 = 10;
+            Console.WriteLine($"{mynumber1} {mynumber2}");
+            multeb_number(ref mynumber1, ref mynumber2);
+            Console.WriteLine($"{mynumber1} {mynumber2}");
+            Console.ReadKey();
+
+        }
+    }
+}
