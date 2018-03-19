849bfedb7817b3ca8da6e63d64152051ee65d915
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-19 04:40:52 +0200

    مدي المتغيرات داخل الهياكل البرمجيه

---
+++ Variable Scope in Structures مدى المتغيرات/Variable Scope in Structures مدى المتغيرات/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable_Scope_in_Structures_مدى_المتغيرات
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+            for (int i = 0; i<10; i++)
+            {
+                 index = i;
+                Console.WriteLine($"{index}");
+            }
+            Console.WriteLine($"the last index = {index}");
+            Console.ReadKey();
+        }
+    }
+}
