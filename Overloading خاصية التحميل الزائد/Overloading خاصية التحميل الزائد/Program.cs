3214b3c3340d4bc4f4d83a627cb01709b71a9b41
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-20 03:47:20 +0200

    التحدي29

---
+++ Overloading خاصية التحميل الزائد/Overloading خاصية التحميل الزائد/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_خاصية_التحميل_الزائد
+{
+    class Program
+    {
+        //public static int printnumber (int number)
+        //{
+        //    return number;
+        //}
+        //public static double printnumber (double number)
+        //{
+        //    return number;
+        //}
+
+            public static int minivalue (int [] array)
+        {
+            int myminivalue = array[0];
+            for (int i =1; i<array.Length; i++)
+            {
+                if (array[i] < myminivalue)
+                    myminivalue = array[i];
+                 
+            }
+            return (myminivalue);
+            
+        }
+        public static double minivalue (double [] array)
+        {
+            double myminivalue = array[0];
+            for (int i=0; i<array.Length; i++)
+            {
+                if (array[i] < myminivalue)
+                    myminivalue = array[i];
+
+            }
+            return myminivalue;
+        }
+        static void Main(string[] args)
+        {
+            int[] myarray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            double[] myarray1 = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.10 };
+            int minivalu = minivalue(myarray);
+            double minivalu1 = minivalue(myarray1);
+            
+            Console.WriteLine($"{minivalu} \n {minivalu1}");
+            Console.ReadKey();
+
+            //int number = 9;
+            //Console.WriteLine($"{printnumber(number)}");
+            //Console.ReadKey();
+
+        }
+    }
+}
