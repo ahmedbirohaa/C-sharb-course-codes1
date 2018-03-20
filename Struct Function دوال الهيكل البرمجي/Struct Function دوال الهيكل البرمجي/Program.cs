485a49afbdcb9adeda65b1c09b8dae040fc9325c
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-20 02:38:12 +0200

    دوال الهيكل البرمجي

---
+++ Struct Function دوال الهيكل البرمجي/Struct Function دوال الهيكل البرمجي/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Function_دوال_الهيكل_البرمجي
+{
+    class Program
+    {
+        struct fullname
+        {
+            public string myfirstname, mylastname;
+            public string print() => myfirstname + "  " + mylastname; 
+        }
+        static void Main(string[] args)
+        {
+            fullname myfullname;
+            myfullname.myfirstname = "ahmed";
+            myfullname.mylastname = "shmah";
+           // Console.WriteLine($"{myfullname.myfirstname}  {myfullname.mylastname}");
+            Console.WriteLine($"{myfullname.print()}");
+            Console.ReadKey();
+        }
+    }
+}
