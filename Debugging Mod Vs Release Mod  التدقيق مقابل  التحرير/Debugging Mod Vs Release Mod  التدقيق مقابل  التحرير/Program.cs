d42c7b8cfdeb323834c4995c53c4fe6f1583c7e2
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-23 03:56:28 +0200

    التدقيق مقابل التحرير

---
+++ Debugging Mod Vs Release Mod  التدقيق مقابل  التحرير/Debugging Mod Vs Release Mod  التدقيق مقابل  التحرير/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Diagnostics;
+
+namespace Debugging_Mod_Vs_Release_Mod__التدقيق_مقابل__التحرير
+{
+    class Program
+    {
+        public static int add(int x, int y)
+        {
+            Debug.WriteLine("we starat add");
+            int z = x + y;
+            Debug.WriteLine("we are done here in add");
+            Trace.WriteLine("we are done here in add");
+
+            return z;  
+        }
+        public static int mult(int x, int y)
+        {
+            Debug.WriteLine("we start mult");
+            int z = x * y;
+            Debug.WriteLine("we are done here in mult");
+            return z;
+        }
+        public static void write(int z)
+        {
+            Debug.WriteLine("we start write");
+            WriteLine($"{z}");
+            Debug.WriteLine("we are done here in write");
+        }
+        static void Main(string[] args)
+        {
+            int mynum1, mynum2 = 4, mynum3 = 7;
+            mynum1 = add(mynum2, mynum3);
+            write(mynum1);
+            mynum1 = mult(mynum2, mynum3);
+            write(mynum1);
+            ReadKey();
+
+        }
+    }
+}
