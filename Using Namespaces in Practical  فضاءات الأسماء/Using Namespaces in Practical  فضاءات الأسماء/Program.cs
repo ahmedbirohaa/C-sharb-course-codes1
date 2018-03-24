bb5366107421bffcc42bb214e00ae656b5a59b48
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-24 08:34:11 +0200

    فضاءات الاسماء

---
+++ Using Namespaces in Practical  فضاءات الأسماء/Using Namespaces in Practical  فضاءات الأسماء/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using yb= brothers.yong_brothers.brothers;
+using ob= brothers.old_brothers.brothers;
+
+namespace Using_Namespaces_in_Practical__فضاءات_الأسماء
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("thes is my family");
+            yb.print();
+            ob.print();
+            ReadKey();
+        }
+    }
+}
+namespace brothers
+{
+    namespace yong_brothers
+    {
+        class brothers
+        {
+            public static void print()
+            {
+                WriteLine("ahmed and mohamed");
+            }
+        }
+    }
+    namespace old_brothers
+    {
+        class brothers
+        {
+            public static void print()
+            {
+                WriteLine("amar and abdalrhman");
+            }
+               
+        }
+    }
+    
+
+}
