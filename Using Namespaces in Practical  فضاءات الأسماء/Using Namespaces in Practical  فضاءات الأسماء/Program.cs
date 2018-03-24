d70e56aca58c0290bb6c41cc34eaab2a22ba934e
ahmedbirohaa <37089324+ahmedbirohaa@users.noreply.github.com>
2018-03-24 08:49:49 +0200

    مكتبات الربط الديناميكي

--- Using Namespaces in Practical  فضاءات الأسماء/Using Namespaces in Practical  فضاءات الأسماء/Program.cs
+++ Using Namespaces in Practical  فضاءات الأسماء/Using Namespaces in Practical  فضاءات الأسماء/Program.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Console;
-using yb= brothers.yong_brothers.brothers;
-using ob= brothers.old_brothers.brothers;
+﻿using static System.Console;
+using yb = brothers.yong_brothers.brothers;
+using ob = brothers.old_brothers.brothers;
 
 namespace Using_Namespaces_in_Practical__فضاءات_الأسماء
 {
@@ -19,30 +14,4 @@ namespace Using_Namespaces_in_Practical__فضاءات_الأسماء
             ReadKey();
         }
     }
-}
-namespace brothers
-{
-    namespace yong_brothers
-    {
-        class brothers
-        {
-            public static void print()
-            {
-                WriteLine("ahmed and mohamed");
-            }
-        }
-    }
-    namespace old_brothers
-    {
-        class brothers
-        {
-            public static void print()
-            {
-                WriteLine("amar and abdalrhman");
-            }
-               
-        }
-    }
-    
-
 }
---
+++ Using Namespaces in Practical  فضاءات الأسماء/oldbrothers/prothers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace brothers
+{
+   
+    namespace old_brothers
+    {
+        public class brothers
+        {
+            public static void print()
+            {
+                WriteLine("amar and abdalrhman");
+            }
+
+        }
+    }
+
+
+}
+
